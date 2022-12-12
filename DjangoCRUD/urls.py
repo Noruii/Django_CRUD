4b0d49d6b75fc9196e8c663a194daed153ae2412
Norbert <Norbertfenix@hotmail.com>
2022-12-11 21:16:05 -0400

    Creating sign up form

--- DjangoCRUD/urls.py
+++ DjangoCRUD/urls.py
@@ -19,5 +19,7 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.helloWorld)
+    path('', views.helloWorld),
+    path('signup/', views.helloWorld)
+
 ]
