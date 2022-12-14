28c594e073f08ddb6b554d074d3c7639e9ae4e59
Norbert <Norbertfenix@hotmail.com>
2022-12-13 23:36:19 -0400

    task form

--- DjangoCRUD/urls.py
+++ DjangoCRUD/urls.py
@@ -22,7 +22,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('tasks/', views.tasks, name='tasks'),
+    path('tasks/create/', views.create_task, name='create_task'),
     path('logout/', views.signout, name='logout'),
-    path('signin/', views.signin, name='signin')
+    path('signin/', views.signin, name='signin'),
 
 ]
